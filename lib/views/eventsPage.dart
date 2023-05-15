c35feba6ad30f5383648730830279d3de87aa21f
Olingo <195367@ecam.be>
2023-05-15 03:08:27 +0200

    added new navigator and fixed back button to pop new navigator

--- lib/views/eventsPage.dart
+++ lib/views/eventsPage.dart
@@ -32,56 +32,63 @@ class _EventsPageState extends State<EventsPage> {
     Navigator.of(context).pushNamed('/addEvent');
     print("add event");
   }
-
+final navigatorKey = GlobalKey<NavigatorState>();
 @override
   Widget build(BuildContext context) {
-    return Navigator(
-      initialRoute: '/',
-      onGenerateRoute: (RouteSettings settings) {
-        WidgetBuilder builder;
-        switch (settings.name) {
-          case '/':
-            builder = (BuildContext context) => Scaffold(
-              appBar: AppBar(
-                title: const Text('Events'),
-              ),
-              body: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: Row(
-                      children: [
-                        Column(
-                          children: <Widget>[
-                            Text("${selectedDate.toLocal()}".split(' ')[0]),
-                            const SizedBox(height: 20.0,),
-                            ElevatedButton(
-                              onPressed: () => _selectDate(context),
-                              child: const Text('Select date'),
-                            ),
-                          ],
-                        ),
-                        ElevatedButton(onPressed: ()=> Navigator.of(context).pushNamed('/addEvent') , child: const Text('Add an event'))
-                      ],
+    return WillPopScope(
+      onWillPop: () async => !await navigatorKey.currentState!.maybePop(),
+      child: Navigator(
+        key: navigatorKey,
+        initialRoute: '/',
+        onGenerateRoute: (RouteSettings settings) {
+          WidgetBuilder builder;
+          switch (settings.name) {
+            case '/':
+              builder = (BuildContext context) => Scaffold(
+                appBar: AppBar(
+                  title: const Text('Events'),
+                ),
+                body: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Row(
+                        children: [
+                          Column(
+                            children: <Widget>[
+                              Text("${selectedDate.toLocal()}".split(' ')[0]),
+                              const SizedBox(height: 20.0,),
+                              ElevatedButton(
+                                onPressed: () => _selectDate(context),
+                                child: const Text('Select date'),
+                              ),
+                            ],
+                          ),
+                          ElevatedButton(
+                            // onPressed: ()=> Navigator.of(context).pushNamed('/addEvent'),
+                            onPressed: () => navigatorKey.currentState!.pushNamed('/addEvent'),
+                            child: const Text('Add an event'))
+                        ],
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    flex: 4,
-                    child: EventList(date: selectedDate, userId: widget.userId),
-                  ),
-                ],
-              ),
-            );
-            break;
-          case '/addEvent':
-            builder = (BuildContext context) => Text("data");
-            break;
-          default:
-            throw Exception('Invalid route: ${settings.name}');
-        }
-        return MaterialPageRoute(builder: builder, settings: settings);
-      },
+                    Expanded(
+                      flex: 4,
+                      child: EventList(date: selectedDate, userId: widget.userId),
+                    ),
+                  ],
+                ),
+              );
+              break;
+            case '/addEvent':
+              builder = (BuildContext context) => Text("data");
+              break;
+            default:
+              throw Exception('Invalid route: ${settings.name}');
+          }
+          return MaterialPageRoute(builder: builder, settings: settings);
+        },
+      ),
     );
   }
 }
