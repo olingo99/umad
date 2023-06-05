355e8e9ccfdb51b22ccb505567a7370750e70b61
Olingo <195367@ecam.be>
2023-06-05 19:24:20 +0200

    added comments to event veiwer

--- lib/widgets/EventViewer.dart
+++ lib/widgets/EventViewer.dart
@@ -11,55 +11,51 @@ class EventViewer extends StatelessWidget {
   final bool friend;  //if the event is displayed in the friend page
   EventViewer({super.key, required this.event, required this.notifyParent, this.friend = false});
 
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {  
     return FutureBuilder(
-      future : CategoryService().getCategoryById(event.iduser, event.idcategory),
+      future : CategoryService().getCategoryById(event.iduser, event.idcategory),             //get the category of the event
       builder: (context, snapshot) {
-        if(snapshot.hasData) {
+        if(snapshot.hasData) {                                                                 //if the category is loaded
           Category category = snapshot.data ?? Category(idcategory: 0, name: "error", iduser: 0);
-          return Container(
+          return Container(                                                                    //display the event, container to have a border and to add podding/margin
             margin: const EdgeInsets.symmetric(horizontal: 1, vertical: 4),
             padding: const EdgeInsets.all(2),
-            decoration: BoxDecoration(
+            decoration: BoxDecoration(                                                        //border
               border: Border.all(color: Colors.black45),
               borderRadius: BorderRadius.circular(5.0),
             ),
-            child: Row(
-              // mainAxisAlignment: MainAxisAlignment.start,
+            child: Row(                                                                       //row to display the event
               children: [
                 Expanded(
                   flex:5,
-                  child: Column(
+                  child: Column(                                                              //column to display the name and category of the event
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(event.name, style: const TextStyle(fontSize: 20)),
-                      Text("Category :  ${category.name}", style: const TextStyle(fontSize: 15, color: Colors.grey)),
+                      Text(event.name, style: const TextStyle(fontSize: 20)),               //name of the event
+                      Text("Category :  ${category.name}", style: const TextStyle(fontSize: 15, color: Colors.grey)), //category of the event
                     ],
                   ),
                 ),
-                Text(event.weight.toString(), style: TextStyle(fontSize: 20, color: event.weight > 0 ? Colors.green : Colors.red)),
-                friend? Container():Padding(
+                Text(event.weight.toString(), style: TextStyle(fontSize: 20, color: event.weight > 0 ? Colors.green : Colors.red)), //weight of the event (green if positive, red if negative) outside the column
+                friend? Container():Padding(                                                //if the event is displayed in the friend page, don't display the delete button
                   padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(onPressed: (){
-                    eventService.deleteEvent(event).then((value) => notifyParent(),);
-                  }, child: const Icon(Icons.delete)),
+                  child: ElevatedButton(onPressed: (){                                    //delete button
+                    eventService.deleteEvent(event).then((value) => notifyParent(),);     //delete the event and refresh the page
+                  }, child: const Icon(Icons.delete)),  
                 ),
               ],
             ),
           );
         }
-        if(snapshot.hasError) {
-          return Text("${snapshot.error}");
+        if(snapshot.hasError) {                                                              
+          return Text("${snapshot.error}");                                              //display the error if there is one
         }
-        return const CircularProgressIndicator();
+        return const CircularProgressIndicator();                                      //display a loading indicator while waiting for the category to load
       }
     );
   }
 
-
-  // Widget deleteButton(){
-
-  // }
 }
