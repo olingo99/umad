4d1ed38dd3d61a5c69b37752bf78e63158aa16dd
Olingo <195367@ecam.be>
2023-06-05 21:40:56 +0200

    added comments to cosntants and http wrapper + added async to put and delete wrapper

--- lib/constanst.dart
+++ lib/constanst.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
+
+// class containing all the constants used in the app, makes it easier to change them
 class Constants{
+
   // static const String API_URL = 'http://localhost:3000';
-
   // static const String API_URL = 'http://pat.infolab.ecam.be:60840';
-  static const String API_URL = 'https://pat.infolab.ecam.be:64340';
+  static const String API_URL = 'https://pat.infolab.ecam.be:64340';  //url of the API
 
-
+  //Colors used in the app, not currently used as darktheme colors are used instead
   Color colorLink = const Color(0xFF0e131f);
   Color colorText = const Color(0xFFc8c8c8);
   Color colorBackground = const Color(0xFF30343f);
@@ -15,39 +16,32 @@ class Constants{
   Color colorHover = const Color(0xCBc03121);
   Color colorTopbar = const Color(0xFF0e131f);
   Color colorSelected = const Color(0xFF614343);
-
-
-
-
-
 }
 
 
+
+//Create a custom dark theme for the app based on the built-in dark theme
 class CustomDarkTheme{
-
-
-
-    static final darkTheme = ThemeData.dark().copyWith(
-    snackBarTheme: SnackBarThemeData(
-      backgroundColor: Constants().colorBackground,
-      contentTextStyle: const TextStyle(
+  static final darkTheme = ThemeData.dark().copyWith(   //copy the dark theme with some modifications
+  snackBarTheme: SnackBarThemeData(                    //change the color of the snackbar
+    backgroundColor: Constants().colorBackground,
+    contentTextStyle: const TextStyle(
+      color: Colors.white,
+    ),
+    elevation: 20,
+    actionTextColor: Constants().colorButton,
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(         //change the style of the buttons
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Constants().colorButton,
+      textStyle: const TextStyle(
         color: Colors.white,
       ),
       elevation: 20,
-      actionTextColor: Constants().colorButton,
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Constants().colorButton,
-        textStyle: const TextStyle(
-          color: Colors.white,
-        ),
-        elevation: 20,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
       ),
     ),
-
+  ),
   );
 }
--- lib/httpServiceWrapper.dart
+++ lib/httpServiceWrapper.dart
@@ -1,77 +1,60 @@
-// import 'dart:html';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'constanst.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+
+// Wrapper for the http service, adds the token and the content-type to the header of the request
 class HttpServiceWrapper {
-  static const String baseUrl = Constants.API_URL;
-  static const Map<String, String> contentTypeHeader = {
+  static const String baseUrl = Constants.API_URL;  //Get the url of the API from the constants file
+  static const Map<String, String> contentTypeHeader = {  //Create the headers for the request, contains the content-type
     'Content-Type': 'application/json; charset=UTF-8',
   };
 
   final storage = new FlutterSecureStorage();
-  // static const noAuthUrl =  [
-  //   {'url': '/login', 'method': 'POST'},
-  //   {'url': '/user/name/', 'method': 'GET'},
-  //   {'url': '/user', 'method': 'POST'},
-  // ];
 
 
-  // Future<http.Response> get(String path) async {
-  //   final url = Uri.parse('$baseUrl$path');
-  //   Map<String, String>headers = {};
-  //   print(headers);
-  //   storage.read(key: 'token').then((value) => headers['Authorization'] = value??'');
-  //   print(headers);
-  //   return await http.get(url, headers: headers);
-  // }
-
-
+  //wrapper for the http get request
   Future<http.Response> get(String path) async {
-    final url = Uri.parse('$baseUrl$path');
-    Map<String, String>headers = {};
-    return storage.read(key: 'token').then((value)async {
-      headers['Authorization'] = value??'';
-      return await http.get(url, headers: headers);
+    final url = Uri.parse('$baseUrl$path');                       //create the url of the request
+    Map<String, String>headers = {};                              //create the headers of the request
+    return storage.read(key: 'token').then((value)async {         //get the token from the storage
+      headers['Authorization'] = value??'';                       //add the token to the headers, uses an empty string if the token is null
+      return await http.get(url, headers: headers);               //returns the future of the request
   });
   }
 
+  //wrapper for the http post request
   Future<http.Response> post(String path, Map<String,dynamic> body) async{
-    final url = Uri.parse('$baseUrl$path');
-    Map<String, String>headers = {};
-    return storage.read(key: 'token').then((value)async {
-      headers['Authorization'] = value??'';
-      headers.addAll(contentTypeHeader);
-      return await http.post(url, headers: headers, body: jsonEncode(body));
+    final url = Uri.parse('$baseUrl$path');                       //create the url of the request
+    Map<String, String>headers = {};                              //create the headers of the request
+    return storage.read(key: 'token').then((value)async {         //get the token from the storage
+      headers['Authorization'] = value??'';                       //add the token to the headers, uses an empty string if the token is null
+      headers.addAll(contentTypeHeader);                          //add the content-type to the headers             
+      return await http.post(url, headers: headers, body: jsonEncode(body));  //returns the future of the request
   });
   }
 
-  Future<http.Response> put(String path, Map<String, dynamic> body){
-    final url = Uri.parse('$baseUrl$path');
-    Map<String, String>headers = {};
-    return storage.read(key: 'token').then((value)async {
-      headers['Authorization'] = value??'';
-      headers.addAll(contentTypeHeader);
-      return await http.put(url, headers: headers, body: jsonEncode(body));
+  //wrapper for the http put request
+  Future<http.Response> put(String path, Map<String, dynamic> body) async { 
+    final url = Uri.parse('$baseUrl$path');                   //create the url of the request                  
+    Map<String, String>headers = {};                          //create the headers of the request                
+    return storage.read(key: 'token').then((value)async {     //get the token from the storage                  
+      headers['Authorization'] = value??'';                   //add the token to the headers, uses an empty string if the token is null                  
+      headers.addAll(contentTypeHeader);                      //add the content-type to the headers
+      return await http.put(url, headers: headers, body: jsonEncode(body)); //returns the future of the request
   });
   }
 
-  Future<http.Response> delete(String path){
-    final url = Uri.parse('$baseUrl$path');
-    Map<String, String>headers = {};
-    return storage.read(key: 'token').then((value)async {
-      headers['Authorization'] = value??'';
-      headers.addAll(contentTypeHeader);
-      return await http.delete(url, headers: headers);
+  //wrapper for the http delete request
+  Future<http.Response> delete(String path) async {           
+    final url = Uri.parse('$baseUrl$path');                   //create the url of the request
+    Map<String, String>headers = {};                        //create the headers of the request
+    return storage.read(key: 'token').then((value)async {   //get the token from the storage
+      headers['Authorization'] = value??'';                //add the token to the headers, uses an empty string if the token is null    
+      headers.addAll(contentTypeHeader);                 //add the content-type to the headers
+      return await http.delete(url, headers: headers);  //returns the future of the request
   });
   }
 
-  // Future<http.Response> post(String path, Map<String,dynamic> body) async{
-  //   final url = Uri.parse('$baseUrl$path');
-  //   Map<String, String>headers = {};
-  //   storage.read(key: 'token').then((value) => headers['Authorization'] = value??'');
-  //   headers.addAll(contentTypeHeader);
-  //   return await http.post(url, headers:headers, body: jsonEncode(body));
-  // }
 }
